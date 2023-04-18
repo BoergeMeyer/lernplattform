f54d9c029c6e4c6c144c50a8f168e53b0f50c450
Börge Meyer <boergermeisterr@icloud.com>
2023-04-18 17:59:55 +0200

    neue Grafiken, Änderunge an HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,156 +14,99 @@
 </head>
 <body>
     <!-- Navigation -->
-    <!-- Icons by Google: https://fonts.google.com/icons -->
-    <div class="container-fluid p-0 fixed-top">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary z-1 position-absolute container-fluid">
         <div class="container-fluid">
-            <img style="height:52px" src="../../public/1200px-HSBA_Hamburg_School_of_Business_Administration_Logo_2018_nur_Wappen.svg.png" class="img-thumbnail px-1" alt="...">
-            <a class="navbar-brand fw-bolder px-2" href="index.html">Digital Labour</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <img style="height:52px" src="../../src/assets/HSBA_Logo.svg" class="px-1" alt="...">
+            <form class="d-flex my-auto mx-4 rounded" role="search">
+                <input class="form-control me-2 rounded-pill fw-bold" type="search" placeholder="Suchen nach..." aria-label="Search">
+            </form>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
+              <li>  
+                <a class="nav-link" href="login.html">
+                    <div class="container text-center">
+                      <div class="row align-items-start">
+                        <div class="col p-0">
+                          <span class="material-symbols-outlined">
+                            home
+                          </span>
+                        </div>
+                        <div class="col fw-bold">
+                            Home                           
+                        </div>
+                      </div>
+                  </div>
+                </a>
+              </li>
+              <li>
                 <a class="nav-link" href="lektion1.html">
-                    <div class="container text-center">
-                        <div class="row align-items-start">
-                            <div class="col p-0">
-                                <span class="material-symbols-outlined">
-                                    school
-                                </span>
-                            </div>
-                            <div class="col fw-bold">
-                                Lektionen                           
-                            </div>
-                        </div>
-                    </div>    
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <div class="container text-center">
-                        <div class="row align-items-start">
-                            <div class="col p-0">
-                                <span class="material-symbols-outlined">
-                                shopping_cart
-                                </span>
-                            </div>
-                            <div class="col fw-bold">
-                                Preisliste                           
-                            </div>
-                        </div>
-                    </div>
-                </a>
-              </li>
-            </ul>
-            <a class="nav-link d-flex" href="login.html">
-                <div class="container text-center">
+                  <div class="container text-center">
                     <div class="row align-items-start">
                         <div class="col p-0">
                           <span class="material-symbols-outlined">
-                            login
-                            </span>
+                            school
+                          </span>
                         </div>
                         <div class="col fw-bold">
-                            Login                           
+                          Kurse                           
                         </div>
+                      </div>
                     </div>
-                </div>
-              </a>
-
-              <button type="button" class="nav-link d-flex" v-if="$store.state.user" @click="$store.dispatch('logout')" >
-                <div class="container text-center">
+                </a>
+              </li>
+              <li>
+                <a class="nav-link" href="login.html">
+                  <div class="container text-center">
                     <div class="row align-items-start">
-                        <div class="col p-0">
-                          <span class="material-symbols-outlined">
-                            logout
-                            </span>
-                        </div>
-                        <div class="col fw-bold">
-                            Logout                           
-                        </div>
+                      <div class="col p-0">
+                        <span class="material-symbols-outlined">
+                          login
+                        </span>
+                      </div>
+                      <div class="col fw-bold">
+                        Login                           
+                      </div>
                     </div>
-                </div>
-              </button>
-              <a class="nav-link d-flex" href="register.html">
-                <div class="container text-center">
-                  <div class="row align-items-start">
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a class="nav-link" href="register.html">
+                  <div class="container text-center">
+                    <div class="row align-items-start">
                       <div class="col p-0">
                         <span class="material-symbols-outlined">
                           input
-                          </span>
+                        </span>
                       </div>
-                      <div class="col fw-bold">
-                        Regstrierung                           
+                        <div class="col fw-bold">
+                            Regstrierung                           
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              </a>
-          </div>
+                  </a>
+              </li>
+            </ul>
         </div>
-      </nav> 
+    </nav>
+
+    <!-- Header -->
+    <div class="" style="height: 100%; background-image: url('../../src/assets/header.svg'); background-repeat: no-repeat; background-position: 50%; background-size: cover;">
+        <div class="position-absolute" style="top: 25%; left: 25px;">
+            <p class="text-start fw-bold" style="color: 002B5C; font-size: 75px;">
+              Lorem Ipsum <br>
+              Lorem Ipsum Lorem <br>
+              Ipsum
+            </p>
+            <a type="button" class="btn btn-primary fs-3 fw-bold rounded-pill px-5" style="background-color: #002B5C; border: none;">KLICKEN</a>
+        </div>
     </div>
 
-    <div class="container-fluid" style="height: 100%; background-image: url('header.png');">
-      <div class="position-absolute" style="top: 25%; left: 25px;">
-        <p class="text-start fw-bold" style="color: 002B5C; font-size: 75px;">
-          Lorem Ipsum <br>
-          Lorem Ipsum Lorem Ipsum
-        </p>
-        <a type="button" class="btn btn-primary" style="font-size: 35px; border-radius: 40px;">KLICK!</a>
-      </div>
+    <!-- Categories -->
+    <div class="" style="height: 100%; background-image: url('../../src/assets/categories.svg'); background-repeat: no-repeat; background-position: 50%; background-size: cover;">
+            
     </div>
-    <!-- Cards -->
-    <div class="container-fluid py-2" style=" background-color: 002B5C">
-      <div class="container text-center">
-        <div class="row py-2">
-          <div class="col">
-            <div class="card mx-auto" style="width: 18rem;">
-              <img src="../../public/Python.svg.png" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Informationen</a>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card mx-auto" style="width: 18rem;">
-              <img src="../../public/Python.svg.png" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Informationen</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row py-2">
-          <div class="col">
-            <div class="card mx-auto" style="width: 18rem;">
-              <img src="../../public/Python.svg.png" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Informationen</a>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card mx-auto" style="width: 18rem;">
-              <img src="../../public/Python.svg.png" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Informationen</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+
+
 </body>
 </template>
 
